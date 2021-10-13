8e03169e0e2e56dbb8308933ac0f394e02c23389
Prince Segbefia <princeasegbefia@gmail.com>
2021-10-13 00:02:40 -0600

    Forecast updated

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,13 +3,16 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=ba98893581f5e80195b6a85da78c5046&query=' + latitude + ',' + longitude + '&units=f'
 
-    request({ url, json: true }, (error, {body}) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+            callback(
+                undefined, 
+                body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + 
+                ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. Humidity is ' + body.current.humidity + '%.')
         }
     })
 
